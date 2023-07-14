d21195d73694e75f64918da073a8905137448dba
Natan <nkeddem@gmail.com>
2023-07-14 08:58:02 -0400

    added minimal logging as standard

--- mylogging.py
+++ mylogging.py
@@ -5,6 +5,7 @@ lastinfo_path = "logs/lastinfo.log"
 lastdebug_path = "logs/lastdebug.log"
 info_path = "logs/info.log"
 warn_path = "logs/warning.log"
+
 try:
     os.remove(lastinfo_path)
 except OSError:
@@ -13,59 +14,101 @@ try:
     os.remove(lastdebug_path)
 except OSError:
     pass
-LOGGING_CONFIG = {
-    "version": 1,
-    "disable_existing_loggers": True,
-    "loggers": {
-        "": {
-            "level": "DEBUG",
-            "handlers": ["console", "all_warning", "all_info", "last_info", "last_debug"],
+
+
+def is_docker():
+    path = "/proc/self/cgroup"
+    status = os.path.exists("/.dockerenv") or os.path.isfile(path) and any("docker" in line for line in open(path))
+    return status
+
+
+verbose_logging = os.environ.get("VERBOSE_LOGGING", False)
+if is_docker() is False or verbose_logging is not False:
+    logging_mode = " Verbose"
+    LOGGING_CONFIG = {
+        "version": 1,
+        "disable_existing_loggers": True,
+        "loggers": {
+            "": {
+                "level": "DEBUG",
+                "handlers": ["console", "all_warning", "all_info", "last_info", "last_debug"],
+            },
         },
-    },
-    "handlers": {
-        "console": {
-            "level": "WARNING",
-            "formatter": "fmt",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stdout",
+        "handlers": {
+            "console": {
+                "level": "WARNING",
+                "formatter": "fmt",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+            },
+            "all_warning": {
+                "level": "WARNING",
+                "formatter": "fmt",
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": warn_path,
+                "maxBytes": 1048576,
+                "backupCount": 10,
+            },
+            "all_info": {
+                "level": "INFO",
+                "formatter": "fmt",
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": info_path,
+                "maxBytes": 1048576,
+                "backupCount": 10,
+            },
+            "last_info": {
+                "level": "INFO",
+                "formatter": "fmt",
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": lastinfo_path,
+                "maxBytes": 1048576,
+            },
+            "last_debug": {
+                "level": "DEBUG",
+                "formatter": "fmt",
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": lastdebug_path,
+                "maxBytes": 1048576,
+            },
         },
-        "all_warning": {
-            "level": "WARNING",
-            "formatter": "fmt",
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": warn_path,
-            "maxBytes": 1048576,
-            "backupCount": 10,
+        "formatters": {
+            "fmt": {"format": "%(asctime)s-%(levelname)s-%(name)s-%(process)d::%(module)s|%(lineno)s:: %(message)s"},
         },
-        "all_info": {
-            "level": "INFO",
-            "formatter": "fmt",
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": info_path,
-            "maxBytes": 1048576,
-            "backupCount": 10,
+    }
+else:
+    logging_mode = ""
+    LOGGING_CONFIG = {
+        "version": 1,
+        "disable_existing_loggers": True,
+        "loggers": {
+            "": {
+                "level": "DEBUG",
+                "handlers": ["console", "all_warning"],
+            },
         },
-        "last_info": {
-            "level": "INFO",
-            "formatter": "fmt",
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": lastinfo_path,
-            "maxBytes": 1048576,
+        "handlers": {
+            "console": {
+                "level": "WARNING",
+                "formatter": "fmt",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+            },
+            "all_warning": {
+                "level": "WARNING",
+                "formatter": "fmt",
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": warn_path,
+                "maxBytes": 1048576,
+                "backupCount": 10,
+            },
         },
-        "last_debug": {
-            "level": "DEBUG",
-            "formatter": "fmt",
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": lastdebug_path,
-            "maxBytes": 1048576,
+        "formatters": {
+            "fmt": {"format": "%(asctime)s-%(levelname)s-%(name)s-%(process)d::%(module)s|%(lineno)s:: %(message)s"},
         },
-    },
-    "formatters": {
-        "fmt": {"format": "%(asctime)s-%(levelname)s-%(name)s-%(process)d::%(module)s|%(lineno)s:: %(message)s"},
-    },
-}
+    }
 dictConfig(LOGGING_CONFIG)
 import logging
 
 logger = logging.getLogger(__name__)
-logger.info(f"***Logging Started***")
+logger.warn(f"***Logging{logging_mode} Started***")
