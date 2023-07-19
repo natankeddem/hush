f8e86d4d52ab3d144df805a3c6f0e8b2b39c334b
Natan <nkeddem@gmail.com>
2023-07-19 16:17:21 -0400

    made smart drive temp regex more generic

--- hardware/smart.py
+++ hardware/smart.py
@@ -31,7 +31,7 @@ class Smart(Device):
     def get_drive_temp(self, drive_path):
         try:
             output = self.ssh_cmd(f"smartctl -l scttemp {drive_path}")
-            temp = re.search(r"Current [^T]*Temperature:\s+(\d+)", output)
+            temp = re.search(r"Current\s*[^T]*Temperature:\s*(\d+)", output)
             if temp is not None and temp.lastindex == 1:
                 return int(temp.group(1))
             else:
