f5f70d3059a3306d7484af9bb502026040cfbfeb
Natan <nkeddem@gmail.com>
2023-07-19 20:17:19 -0400

    improved smart logging and compatibility

--- hardware/smart.py
+++ hardware/smart.py
@@ -11,10 +11,11 @@ class Smart(Device):
     def __init__(self, address, password, username="root"):
         super().__init__(address=address, password=password, username=username)
         self.base_cmd = f"sshpass -p {self._password} ssh -o StrictHostKeychecking=no {self._username}@{self._address}"
+        self._full_cmd = ""
 
     def ssh_cmd(self, command):
-        full_cmd = f"{self.base_cmd} {command}"
-        value = subprocess.run(shlex.split(full_cmd), capture_output=True, timeout=5)
+        self._full_cmd = f"{self.base_cmd} {command}"
+        value = subprocess.run(shlex.split(self._full_cmd, posix=False), capture_output=True, timeout=5)
         value = value.stdout.decode("utf-8")
         return value
 
@@ -24,20 +25,26 @@ class Smart(Device):
             output = self.ssh_cmd("fdisk -l")
             drive_paths = re.findall(r"Disk (\/dev\/sd[a-z]+|\/dev\/nvm[0-9]+n[0-9]+)", output)
         except Exception as e:
-            logger.error(f"{self._address} failed to get cpu temperature from: {output}")
+            logger.info(f"{self._address} failed to get drive list:")
+            logger.info(f"cmd = {self._full_cmd}")
+            logger.info(f"output = {output}")
             raise e
         return drive_paths
 
     def get_drive_temp(self, drive_path):
         try:
-            output = self.ssh_cmd(f"smartctl -x {drive_path}")
+            output = self.ssh_cmd(f'smartctl -x {drive_path} | grep -E "Temp|Cel|Cur|temp|cel|cur"')
             temp = re.search(r"Current\s*[^T]*Temperature:\s*(\d+)", output)
             if temp is not None and temp.lastindex == 1:
                 return int(temp.group(1))
             else:
-                logger.info(f"{self._address} failed to get drive temperature from: {drive_path}")
+                logger.info(f"{self._address} failed to get drive temperature {drive_path}:")
+                logger.info(f"cmd = {self._full_cmd}")
+                logger.info(f"output = {output}")
         except Exception as e:
-            logger.info(f"{self._address} failed to get drive temperature from: {drive_path}")
+            logger.info(f"{self._address} failed to get drive temperature {drive_path}:")
+            logger.info(f"cmd = {self._full_cmd}")
+            logger.info(f"output = {output}")
             raise e
 
     def get_temp(self):
@@ -51,7 +58,8 @@ class Smart(Device):
             self._temp = int(np.mean(drive_temps))
             return self._temp
         except Exception as e:
-            logger.info(f"{self._address} failed | drive_paths={drive_paths} | drive_temps={drive_temps}")
+            logger.info(f"drive_paths = {drive_paths}")
+            logger.info(f"drive_temps = {drive_temps}")
             raise e
 
 
