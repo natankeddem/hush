79c56163110465105ee0550cdd6d46d0c082290f
Natan <nkeddem@gmail.com>
2023-07-28 20:01:47 -0400

    added cisco m series sliders

--- tabs/algorithms.py
+++ tabs/algorithms.py
@@ -152,6 +152,62 @@ class iDrac9Sliders(Sliders):
             app.storage.general[configs_version_string] = configs.to_dict()
 
 
+class CiscoSliders(Sliders):
+    def add(self):
+        n = self._expansion.parent_slot.name
+        default_tab = None
+        with ui.tabs().classes("w-full") as curve_tabs:
+            if configs[n].get("cpu_temp_type", "None") != "None":
+                cpu_curve_tab = ui.tab("CPU")
+                default_tab = cpu_curve_tab
+            if configs[n].get("drive_temp_type", "None") != "None":
+                drives_curve_tab = ui.tab("Drives")
+                default_tab = drives_curve_tab
+            if configs[n].get("gpu_temp_type", "None") != "None":
+                gpu_curve_tab = ui.tab("GPU")
+                default_tab = gpu_curve_tab
+        with ui.tab_panels(curve_tabs, value=default_tab, animated=False).classes("w-full").style("min-height: 300px"):
+            if configs[n].get("cpu_temp_type", "None") != "None":
+                with ui.tab_panel(cpu_curve_tab).classes("w-full"):
+                    with ui.row().classes("w-full items-center justify-around"):
+                        self._add_single(name=n, sensor="cpu", label="Low Power", temp_default=30)
+                        self._add_single(name=n, sensor="cpu", label="Balanced", temp_default=40)
+                        self._add_single(name=n, sensor="cpu", label="Performance", temp_default=50)
+                        self._add_single(name=n, sensor="cpu", label="High Power", temp_default=60)
+                        self._add_single(name=n, sensor="cpu", label="Max Power", temp_default=70)
+            if configs[n].get("drive_temp_type", "None") != "None":
+                with ui.tab_panel(drives_curve_tab).classes("w-full"):
+                    with ui.row().classes("w-full items-center justify-around"):
+                        self._add_single(name=n, sensor="drive", label="Low Power", temp_default=30)
+                        self._add_single(name=n, sensor="drive", label="Balanced", temp_default=40)
+                        self._add_single(name=n, sensor="drive", label="Performance", temp_default=50)
+                        self._add_single(name=n, sensor="drive", label="High Power", temp_default=60)
+                        self._add_single(name=n, sensor="drive", label="Max Power", temp_default=70)
+            if configs[n].get("gpu_temp_type", "None") != "None":
+                with ui.tab_panel(gpu_curve_tab).classes("w-full"):
+                    with ui.row().classes("w-full items-center justify-around"):
+                        self._add_single(name=n, sensor="gpu", label="Low Power", temp_default=30)
+                        self._add_single(name=n, sensor="gpu", label="Balanced", temp_default=40)
+                        self._add_single(name=n, sensor="gpu", label="Performance", temp_default=50)
+                        self._add_single(name=n, sensor="gpu", label="High Power", temp_default=60)
+                        self._add_single(name=n, sensor="gpu", label="Max Power", temp_default=70)
+
+    def _add_single(self, name, sensor, label, temp_default):
+        with ui.column().classes("items-center"):
+            if type(configs[name]["algo"]["curves"][sensor]["temp"][label]) is AdDict:
+                temp_value = temp_default
+                configs[name]["algo"]["curves"][sensor]["temp"][label] = temp_value
+            else:
+                temp_value = configs[name]["algo"]["curves"][sensor]["temp"][label]
+            temp = ui.slider(min=20, max=120, value=temp_value, step=5, on_change=lambda s: self._store_temp(s)).props(
+                "reverse vertical label-always snap label-value='value + 'C''"
+            )
+            temp.default_slot.name = f"{name}.{sensor}.{label}"
+            ui.label(label)
+            configs[name]["algo"]["curves"][sensor]["speed"][label] = label
+            app.storage.general[configs_version_string] = configs.to_dict()
+
+
 class Algorithms(Tab):
     def __init__(self):
         super().__init__()
@@ -169,8 +225,12 @@ class Algorithms(Tab):
         if row is not None:
             with row:
                 with ui.expansion(name, icon="bar_chart").classes("w-full") as expansion:
-                    if configs[name].get("speed_ctrl_type", "None") == "iDRAC 9":
+                    ctrl_type = configs[name].get("speed_ctrl_type", "None")
+                    if ctrl_type == "Dell iDRAC 9":
                         sliders = iDrac9Sliders(expansion)
+                        sliders.add()
+                    if ctrl_type == "Cisco M3" or ctrl_type == "Cisco M4" or ctrl_type == "Cisco M5":
+                        sliders = CiscoSliders(expansion)
                         sliders.add()
                     else:
                         sliders = Sliders(expansion)
