db9d98a7f7ec5fff1bd3649b4db63cf22b7ee02e
Natan Keddem <git@keddem.com>
2024-03-05 19:59:35 -0500

    cleanup

--- main.py
+++ main.py
@@ -16,21 +16,23 @@ else:
     logger.warning("Found 'data' directory.")
 os.environ.setdefault("NICEGUI_STORAGE_PATH", "data")
 
-from nicegui import ui  # type: ignore
 
-ui.card.default_style("max-width: none")
-ui.card.default_props("flat bordered")
-ui.input.default_props("outlined dense hide-bottom-space")
-ui.button.default_props("outline dense")
-ui.select.default_props("outlined dense dense-options")
-ui.checkbox.default_props("dense")
-ui.stepper.default_props("flat")
-ui.stepper.default_classes("full-size-stepper")
+if __name__ in {"__main__", "__mp_main__"}:
+    from nicegui import app, ui  # type: ignore
 
-from hush import page, logo, control
+    ui.card.default_style("max-width: none")
+    ui.card.default_props("flat bordered")
+    ui.input.default_props("outlined dense hide-bottom-space")
+    ui.button.default_props("outline dense")
+    ui.select.default_props("outlined dense dense-options")
+    ui.checkbox.default_props("dense")
+    ui.stepper.default_props("flat")
+    ui.stepper.default_classes("full-size-stepper")
 
-launcher = control.Launcher()
-ui.timer(1, launcher.run)
+    from hush import page, logo, control
+
+    launcher = control.Launcher()
+    ui.timer(1, launcher.run)
 
 if __name__ in {"__main__", "__mp_main__"}:
     page.build()
