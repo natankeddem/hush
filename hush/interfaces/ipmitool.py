690007fbc3e8e0247d72e662f7ac64030de5d91e
Natan Keddem <git@keddem.com>
2025-01-29 18:46:07 -0500

    allow ipmitool interace change

--- hush/interfaces/ipmitool.py
+++ hush/interfaces/ipmitool.py
@@ -1,16 +1,18 @@
 import logging
 
 logger = logging.getLogger(__name__)
+from typing import Literal
 from hush.interfaces import cli
 
 
 class IpmiTool(cli.Cli):
-    def __init__(self, hostname: str, username: str, password: str):
+    def __init__(self, hostname: str, username: str, password: str, interface: Literal["lanplus", "lan"] = "lanplus"):
         super().__init__()
         self._hostname: str = hostname
         self._username: str = username
         self._password: str = password
-        self.base_command = f"ipmitool -I lanplus -H {self._hostname} -U {self._username} -P {self._password}"
+        self._interface: Literal["lanplus", "lan"] = interface
+        self.base_command = f"ipmitool -I {self._interface} -H {self._hostname} -U {self._username} -P {self._password}"
 
     async def execute(self, command: str) -> cli.Result:
         command = f"{self.base_command} {command}"
